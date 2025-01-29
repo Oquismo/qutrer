4959b0601f1ce5cf9f7c6480a2caa36a05de29b1
Oquismo <rovetta215@gmail.com>
2025-01-29 01:15:07 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,27 +4,18 @@ import { getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWith
 import { getFirestore, deleteDoc, doc } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB5CV_ZNWmyDWMbWOBNQjx67AZtO0mvstI",
-//   authDomain: "twitter-clone-edfdb.firebaseapp.com",
-//   projectId: "twitter-clone-edfdb",
-//   storageBucket: "twitter-clone-edfdb.firebasestorage.app",
-//   messagingSenderId: "654030345618",
-//   appId: "1:654030345618:web:8159c5844278b5fad4dd4b",
-//   measurementId: "G-3PSGHERSHP"
-// };
-
-
-
 const firebaseConfig = {
-  apiKey: "EACT_APP_FIREBASE_API_KEY",
-  authDomain: "REACT_APP_FIREBASE_AUTH_DOMAIN",
-  projectId: "REACT_APP_FIREBASE_PROJECT_ID",
-  storageBucket: "REACT_APP_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "REACT_APP_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "EACT_APP_FIREBASE_APP_ID",
-  measurementId: "REACT_APP_FIREBASE_MEASUREMENT_ID"
+  apiKey: "AIzaSyB5CV_ZNWmyDWMbWOBNQjx67AZtO0mvstI",
+  authDomain: "twitter-clone-edfdb.firebaseapp.com",
+  projectId: "twitter-clone-edfdb",
+  storageBucket: "twitter-clone-edfdb.firebasestorage.app",
+  messagingSenderId: "654030345618",
+  appId: "1:654030345618:web:8159c5844278b5fad4dd4b",
+  measurementId: "G-3PSGHERSHP"
 };
+
+
+
 
 
 
