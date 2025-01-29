a6e436286d042e91e2484fbac6f33fcf9d7fb805
Oquismo <rovetta215@gmail.com>
2025-01-29 01:37:36 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,19 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { getFirestore, deleteDoc, doc } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB5CV_ZNWmyDWMbWOBNQjx67AZtO0mvstI",
-  authDomain: "twitter-clone-edfdb.firebaseapp.com",
-  projectId: "twitter-clone-edfdb",
-  storageBucket: "twitter-clone-edfdb.firebasestorage.app",
-  messagingSenderId: "654030345618",
-  appId: "1:654030345618:web:8159c5844278b5fad4dd4b",
-  measurementId: "G-3PSGHERSHP"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-
-
-
-
-
-
-
-
 
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig);
@@ -59,4 +52,5 @@ export const loginWithEmailAndPassword = async (email, password) => {
   } catch (error) {
     console.error("Error al iniciar sesión:", error);
     throw error;
-  }};
+  }
+};
